f5a65ad110291ac801dd3dcf126799ae359fd23a
Dennis Dang <dangggdennis@gmail.com>
2023-06-17 10:45:03 -0400

    solve challenge 0

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,11 +12,12 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
@@ -33,17 +34,21 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 
 	wg.Add(len(urls))
 	for _, u := range urls {
+		<-throttle
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, throttle)
 	}
-	return
 }
 
 func main() {
 	var wg sync.WaitGroup
+	// The crawler is allowed to kick off another goroutine after every second.
+	throttle := time.Tick(time.Second)
 
+	// The WaitGroup is used to wait for all goroutines to finish.
+	// So long as the count is greater than 0, the main goroutine will block with wg.Wait().
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
